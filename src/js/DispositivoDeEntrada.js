fceeccc68d78bcfcc1112e62850e5e6f8db8f809
Christian Marquez <marquezcristian920@gmail.com>
2021-07-15 21:29:13 -0600

    Agregando clase dispositivo de entrada

---
+++ src/js/DispositivoDeEntrada.js
@@ -0,0 +1,25 @@
+class DispositivosDeEntrada {
+  _tipoEntrada;
+  _marca;
+
+  constructor(tipoEntrada, marca) {
+    this._tipoEntrada = tipoEntrada;
+    this._marca = marca;
+  }
+
+  get getTipoEntrada() {
+    return this._tipoEntrada;
+  }
+
+  set setTipoEntrada(tipoEntrada) {
+    this._tipoEntrada = tipoEntrada;
+  }
+
+  get getMarca() {
+    return this._marca;
+  }
+
+  set setMarca(marca) {
+    this._marca = marca;
+  }
+}
